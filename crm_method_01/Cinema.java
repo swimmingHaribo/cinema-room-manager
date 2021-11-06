88bd3d7dd03d637ec175d8a3f3778a847219cbed
Ada <82273499+swimmingAda@users.noreply.github.com>
2021-11-06 23:41:25 +0800

    Add files via upload

---
+++ crm_method_01/Cinema.java
@@ -0,0 +1,22 @@
+package com.crm_method_01;
+
+public class Cinema{
+    public static void main(String[] args) {
+        Room room = new Room(InputUtils.inputRows(), InputUtils.inputSeats());
+        while (true) {
+            switch (InputUtils.inputMenuOption()) {
+                case 1:
+                    room.printRoom();
+                    break;
+                case 2:
+                    while (!room.buyTicket(InputUtils.inputRow(), InputUtils.inputSeat())) ;
+                    break;
+                case 3:
+                    room.caclAndPrintStatistics();
+                    break;
+                case 0:
+                    return;
+            }
+        }
+    }
+}
